e60fe584bfb400cf62bd6d0d774115be37eae741
Sam Liao <phyomh@gmail.com>
2015-09-15 15:13:01 +0800

    parse more than one reader

--- winscard/scard.py
+++ winscard/scard.py
@@ -14,11 +14,21 @@ class SCard:
             raise WinSCardError(result.value)
 
     def list_readers(self, mszGroups=None):
-        nd = LPSTR()
-        result = ULONG(
-            scard_dll.SCardListReadersA(self.context, mszGroups, pointer(nd), pointer(self.dwAutoAllocate)))
+        _pstrReaders = POINTER(c_char)()
+        result = ULONG(scard_dll.SCardListReadersA(self.context, mszGroups, pointer(_pstrReaders), pointer(self.dwAutoAllocate)))
         if not result.value == SCARD_S_SUCCESS:
             raise WinSCardError(result.value)
-        self.reader_names = str(nd.value).split('\r\n')
+        self.reader_names = []
+        # parse returned string array.
+        start = 0
+        cur   = 0
+        while 1:
+            if _pstrReaders[cur] == '\0':
+                self.reader_names.append(_pstrReaders[start:cur])
+                if _pstrReaders[cur+1] == '\0':
+                    break
+                start = cur + 1
+            cur += 1
+        # create reader instances.
         readers = [Reader(LPSTR(x), self.card, self.context) for x in self.reader_names]
         return readers
